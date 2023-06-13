f858debcfd5ef4e43f0d761be23610283ebecc5a
WilcoApp <hello@wilco.gg>
2023-06-13 18:46:46 +0000

    Never Build Permissions Again

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,16 +1,11 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
-import styled from "styled-components";
 
 const Banner = () => {
-  const StyledBanner = styled.div`
-    background-color: ${(props) => props.theme.dark};
-    `
   return (
-    <StyledBanner>
     <div className="banner text-white">
       <div className="container p-4 text-center">
-        <img src={logo} alt="banner" />
+        <img src={logo}/>
         <div>
           <span>A place to </span>
           <span id="get-part">get</span>
@@ -18,7 +13,6 @@ const Banner = () => {
         </div>
       </div>
     </div>
-  </StyledBanner>
   );
 };
 
